b63953079adab1326c88549f124bdc5132120b8a
Vinicius Souza <viniciuspzt@gmail.com>
2023-06-18 00:39:41 +0100

    create main cards, navigate buttons

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,65 @@
 <template>
-  <q-page class="row items-center justify-evenly">
+  <q-page class="column items-left">
+    <q-card class="my-card" flat>
+      <q-card-section horizontal>
+        <div>
+          <q-card-section vertical>
+            <h1>{{ title }}</h1>
+            <p>{{ description }}</p>
+
+            <q-btn @click="navigateToNewArticle" label="Read more" color="primary" />
+          </q-card-section>
+        </div>
+        <q-img class="col-5" src="https://cdn.quasar.dev/img/parallax1.jpg" />
+
+      </q-card-section>
+    </q-card>
+
+
+    <div class="q-pa-md">
+      <p>Categories</p>
+      <q-chip v-for="cat in categories" :key="cat" :label="cat" clickable @click="onClickChip(cat)" outline
+        color="primary" text-color="white" />
+    </div>
+
+    <q-card class="my-card" flat>
+      <q-card-section horizontal>
+        <q-img class="col-5" src="https://cdn.quasar.dev/img/parallax1.jpg" />
+        <div>
+          <q-card-section vertical>
+            <h1>{{ title }}</h1>
+            <p>{{ description }}</p>
+
+            <q-btn @click="navigateToNewArticle" label="Read more" color="primary" />
+          </q-card-section>
+        </div>
+      </q-card-section>
+    </q-card>
+
+
     <example-component title="Example component" active :todos="todos" :meta="meta"></example-component>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 import { Todo, Meta } from 'components/models';
 import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+
+const router = useRouter()
+const title = 'Lorem ipsum dolor sit amet'
+const description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+const categories = ['Marketing', 'Design', 'Engineering']
+
+
+const onClickChip = function (cat: string) {
+  console.log('chip:', cat)
+}
+
+const navigateToNewArticle = () => {
+  router.push({ path: '/new-article' });
+};
 
 const todos = ref<Todo[]>([
   {
